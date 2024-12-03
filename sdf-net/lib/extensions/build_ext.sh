da4102e3697bc613670fad274de3b39b261697ad
Egor-eth <52208679+Egor-eth@users.noreply.github.com>
2024-12-03 14:02:53 +0300

    Bump cpp version to 17

--- sdf-net/lib/extensions/build_ext.sh
+++ sdf-net/lib/extensions/build_ext.sh
@@ -1,4 +1,4 @@
 # Install C++/CUDA extensions
 for ext in mesh2sdf_cuda sol_nglod; do
-    cd $ext && python setup.py clean --all install --user && cd -
+    cd $ext && python setup.py clean --all install && cd -
 done
