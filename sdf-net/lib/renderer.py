f22de26532765309532a7c5488e66f0da783f413
Egor-eth <52208679+Egor-eth@users.noreply.github.com>
2024-12-12 15:19:41 +0300

    Fixed attenuation

--- sdf-net/lib/renderer.py
+++ sdf-net/lib/renderer.py
@@ -191,7 +191,8 @@ class Renderer():
             #print(ray_o.shape)
             #print(ray_lp.shape)
 
-            ilightcolor = (lp[3] * light_color)[None, :] / (ray_lp ** 2).sum(axis=-1)[..., None]
+            attenuation = torch.clamp(10 / (ray_lp ** 2).sum(axis=-1), 0, 1)
+            ilightcolor = (lp[3] * light_color)[None, :] * attenuation[..., None]
 
 
             rb.rgb = torch.clamp(ilightcolor[None, None, :] * torch.clamp((-ray_lp * rb.normal).sum(axis=-1), 0)[..., None] / torch.pi, 0, 1)
